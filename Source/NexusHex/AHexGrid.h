e159749a413aaa010f6fd9dd0db4ef01ac98ea95
Cats2333 <13683050032@163.com>
2025-04-21 13:32:09 +0800

    Initial commit

---
+++ Source/NexusHex/AHexGrid.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+// Core Mass headers
+#include "MassCommonTypes.h"
+#include "MassEntityTypes.h"
+#include "MassArchetypeTypes.h"
+#include "MassEntitySubsystem.h"
+#include "MassProcessor.h"
+#include "MassExecutionContext.h"
+#include "MassCommonFragments.h"
+
+// MassGameplay related
+//#include "MassGameplayDebugTypes.h"
+
+// MassAI related (if needed)
+//#include "MassAIBaseTypes.h"
+
+// MassCrowd related (if needed)
+//#include "MassCrowdTypes.h"
+/**
+ * 
+ */
+class NEXUSHEX_API AHexGrid
+{
+public:
+	AHexGrid();
+	~AHexGrid();
+};
